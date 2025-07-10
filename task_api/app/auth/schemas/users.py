5ec0d94f0e4f93a3f4fe054a0b1b8f1a6c48abce
Artemiy <artemsikor18@gmail.com>
2025-07-11 00:12:00 +0300

    Fix problems with broker and his accessor Add O2Autho protocol.

---
+++ task_api/app/auth/schemas/users.py
@@ -0,0 +1,26 @@
+from pydantic import EmailStr, BaseModel
+from app.base.base_pydantic import Base
+from app.auth.models.enyms import UserStatus
+
+
+class UserSchemaResponse(Base):
+    id: int
+    tg_id: int
+    name: str
+    login: EmailStr
+
+
+class User(Base):
+    id: int
+    tg_id: int
+    name: str
+    login: str
+    hashed_password: str
+    status: UserStatus
+
+
+class CreateUserDTO(Base):
+    tg_id: int
+    name: str
+    login: str
+    password: str
